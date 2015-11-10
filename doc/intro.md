01b3404197f57122b7f869a88cbcb377485ebf09
Sergio Alvarez-Napagao <tranchis@gmail.com>
2015-11-10 23:26:07 +0000

    Move code from photon

---
+++ doc/intro.md
@@ -0,0 +1,3 @@
+# Introduction to photon-redis
+
+TODO: write [great documentation](http://jacobian.org/writing/what-to-write/)
---
+++ project.clj
@@ -0,0 +1,9 @@
+(defproject tranchis/photon-redis "0.9.40"
+  :description "FIXME: write description"
+  :url "http://example.com/FIXME"
+  :license {:name "Eclipse Public License"
+            :url "http://www.eclipse.org/legal/epl-v10.html"}
+  :dependencies [[org.clojure/clojure "1.7.0"]
+                 [tranchis/photon-db "0.9.31"]
+                 [com.github.kstyrc/embedded-redis "0.6"]
+                 [com.taoensso/carmine "2.12.0"]])
